6ddb1daaa459017819cb062f09e8cebba6ebe3a8
Viktoria Radkova <viktoria.radkova01@gmail.com>
2024-06-15 21:08:34 +0300

    fix timing; test everything

--- AutomaticPetFeeder.py
+++ AutomaticPetFeeder.py
@@ -1,14 +1,16 @@
-import asyncio
 import BlynkLib
 import RPi.GPIO as GPIO
 from time import sleep, localtime
+from threading import Timer
 
 BLYNK_AUTH = 'fj4HwAQ1FMCqDW49H2DcHPyZCBO_pn-x' 
 
 GPIO_SERVO_CONTROL_PIN = 11
 GPIO_SERVO_FREQ = 50
-GPIO_SERVO_OPEN = 3
-GPIO_SERVO_CLOSE = 12
+GPIO_SERVO_OPEN = 9
+GPIO_SERVO_CLOSE = 15
+
+SERVO_OPEN_TIME = 1.5
 
 BLYNK_OPERATION_MODE_VPIN = 0
 BLYNK_MANUAL_FEED_VPIN = 1
@@ -18,8 +20,7 @@ BLYNK_EVENING_HOUR_VPIN = 3
 operationMode = 'manual'
 morningScheduleHour = 0
 eveningScheduleHour = 13
-morningFeedTask = None
-eveningFeedTask = None
+feedTimer = None
 
 def initialize_gpio():
     GPIO.setmode(GPIO.BOARD)
@@ -30,36 +31,50 @@ def initialize_gpio():
 
 def run_feed_procedure():
     servoControl.ChangeDutyCycle(GPIO_SERVO_OPEN)   
-    sleep(1)             
+    sleep(SERVO_OPEN_TIME)             
     servoControl.ChangeDutyCycle(GPIO_SERVO_CLOSE) 
     sleep(1)
     servoControl.ChangeDutyCycle(0)
     blynk.log_event('pet_has_been_fed')
 
-def createTimers():
-    morningFeedTask = asyncio.create_task(schedule_timer, 'morning')
-    eveningFeedTask = asyncio.create_task(schedule_timer, 'evening')
+def check_feed_schedule():
+  print('Check feed schedule')
+  global morningScheduleHour
+  global eveningScheduleHour
+  
+  currTime = localtime()
+  
+  hasFed = False
+  
+  print('Check morning time {} == {}'.format(currTime.tm_hour, morningScheduleHour))
+  if currTime.tm_hour == morningScheduleHour:
+    print('Trigger morning feed time at {}:{}'.format(currTime.tm_hour, currTime.tm_min))
+    run_feed_procedure()
+    hasFed = True
+    
+  print('Check evening time {} == {}'.format(currTime.tm_hour, eveningScheduleHour))
+  if currTime.tm_hour == eveningScheduleHour:
+    print('Trigger evening feed time at {}:{}'.format(currTime.tm_hour, currTime.tm_min))
+    run_feed_procedure()
+    hasFed = True
+    
+  createTimer(60 * 60 if hasFed else 5)
+    
 
-async def schedule_timer(scheduleType):
-    while True:
-        currTime = localtime()
-        targetHour = morningScheduleHour
-        
-        if scheduleType == 'evening':
-            targetHour = eveningScheduleHour
-            
-        if currTime.tm_hour == targetHour:
-            run_feed_procedure()
-            await asyncio.sleep(60 * 60)
-            
-        await asyncio.sleep(60)
+def createTimer(sleepTime = 5):
+  print('Create feed timer in {} seconds'.format(sleepTime))
+  global feedTimer
+  
+  feedTimer = Timer(sleepTime, check_feed_schedule)
+  feedTimer.start()
     
-def clearTimers():
-    morningFeedTask.cancel()
-    morningFeedTask = None
-    eveningFeedTask.cancel()
-    eveningFeedTask = None
+def clearTimer():
+    print('Clear feed timer')
+    global feedTimer
     
+    if feedTimer is not None:
+      feedTimer.cancel()
+      feedTimer = None
     
 servoControl = initialize_gpio()
 blynk = BlynkLib.Blynk(BLYNK_AUTH, server='blynk.cloud', log=print)
@@ -69,7 +84,6 @@ def blynk_connected():
     print("Syncing virtual pin values from cloud...")
     blynk.sync_virtual(
         BLYNK_OPERATION_MODE_VPIN,
-        BLYNK_MANUAL_FEED_VPIN,
         BLYNK_MORNING_HOUR_VPIN,
         BLYNK_EVENING_HOUR_VPIN,
     )
@@ -77,37 +91,46 @@ def blynk_connected():
 # Tells Blynk to call the below function when a new value is written for this virtul pin
 @blynk.VIRTUAL_WRITE(BLYNK_OPERATION_MODE_VPIN)
 def on_operation_mode_change(value):
+  global operationMode
+
   if int(value[0]) == 1:
     operationMode = 'automatic'
-    createTimers()
-  else
+    createTimer()
+  else:
     operationMode = 'manual'
-    clearTimers()
+    clearTimer()
       
   print('Operation mode changed to {}'.format(operationMode))
 
 @blynk.VIRTUAL_WRITE(BLYNK_MANUAL_FEED_VPIN)
 def on_manual_feed(value): 
-    if int(value[0]) == 1 && operationMode == 'manual':
+    if int(value[0]) == 1 and operationMode == 'manual':
         print('Performing manual feed...')
         run_feed_procedure()
         
 @blynk.VIRTUAL_WRITE(BLYNK_MORNING_HOUR_VPIN)
 def on_morning_schedule_change(value):   
+    global morningScheduleHour
     morningScheduleHour = int(value[0])
+    clearTimer()
+    createTimer()
 
 @blynk.VIRTUAL_WRITE(BLYNK_EVENING_HOUR_VPIN)
 def on_evening_schedule_change(value):   
+    global eveningScheduleHour
     eveningScheduleHour = int(value[0])
+    clearTimer()
+    createTimer()
 
-#async def main():
+# async def main():
 try:
   while True:
     blynk.run()
-except:
+except err:
   print("An exception occurred. Shutting down ...") 
+  print(err)
   servoControl.stop()         
   GPIO.cleanup() 
 
-#if __name__ == "__main__":
-#    asyncio.run(main())
+# if __name__ == "__main__":
+    # asyncio.run(main())
